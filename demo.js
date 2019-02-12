16aff8d4d2c18abfa9d18b54778a95f1038a573e
Yuxiangbaozi <378928394@qq.com>
2019-02-12 16:34:44 +0800

    commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+var fs = require('fs')
+    var dirName = process.argv[2]
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName)
+    fs.mkdirSync('css')
+    fs.mkdirSync('js')
+    fs.writeFileSync("./index.html", "<!DOCTYPE> <title>Hello</title> <h1>Hi</h1>")
+    fs.writeFileSync("css/style.css", "h1{color: red;}")
+    fs.writeFileSync("./js/main.js", "")                          
+process.exit(0)
